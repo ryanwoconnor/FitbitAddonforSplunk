717b2bba10808dd530f24bda37121bf229da5477
root <root@L1BARCVRWO06OP2.business.uconn.edu>
2018-01-27 01:04:53 -0500

    Added functions for updates and deletes to REST Token as well as functions for refresh token and calling the REST API

--- bin/fitbit_get.py
+++ bin/fitbit_get.py
@@ -40,6 +40,61 @@ def setup_logger(level):
 
     return logger
 
+def RefreshToken(refresh_token):
+    try:
+        #Refresh Token 
+        req = urllib2.Request('https://5876gyevwj.execute-api.us-west-2.amazonaws.com/prod/refreshFitbitToken?code='+refresh_token)
+        response = urllib2.urlopen(req)
+        codes = json.loads(response.read())
+        logger.info(str(codes))
+        return codes
+    except Exception as e:
+        logger.info(str(e))
+
+def DeleteToken(token_id):
+    delete_path = '/servicesNS/nobody/FitbitAddonforSplunk/storage/passwords/' + token_id + ':'+token_id+':'
+    try:
+        serverResponse, serverContent = splunk.rest.simpleRequest(delete_path, sessionKey=sessionKey, method='DELETE',raiseAllErrors=True)
+        logger.info(str(e))
+    except Exception as e:
+        logger.info(str(e))
+
+def UpdateToken(refreshed_tokens_json, token_id):
+    json_built = {'APIKey':refreshed_tokens_json['APIKey'],'RefreshToken':refreshed_tokens_json['RefreshToken']}
+    creds = {"name": token_id, "password": json_built, "realm": token_id}
+    post_path = '/servicesNS/nobody/FitbitAddonforSplunk/storage/passwords?output_mode=json'
+    try:
+        serverResponse = splunk.rest.simpleRequest(post_path, sessionKey=sessionKey, postargs=creds, method='POST',raiseAllErrors=True)
+        logger.info(serverResponse)
+    except Exception as e:
+        logger.info(str(e))
+
+# start the real work
+def GetTokens():
+    proc = []
+    keys_dict = {}
+    get_path = '/servicesNS/nobody/FitbitAddonforSplunk/storage/passwords?output_mode=json'
+    try:
+        serverResponse = splunk.rest.simpleRequest(get_path, sessionKey=sessionKey, method='GET', raiseAllErrors=True)
+    except Exception as e:
+        logger.info(str(e))
+    jsonObj = json.loads(serverResponse[1])
+    my_app = "FitbitAddonforSplunk"
+    if len(jsonObj['entry']) == 0:
+        logger.warn("No credentials found.")
+        sleep(60)
+        sys.exit(0)
+    else:
+        for entry in jsonObj['entry']:
+            if entry['acl']['app'] != my_app:
+                continue
+            if 'clear_password' in entry['content'] and 'username' in entry['content']:
+                keys_dict[entry['content']['username']] = entry['content']['clear_password']
+                tokens = entry['content']['clear_password']
+                token_id = entry['content']['username']
+    return keys_dict
+
+
 logger = setup_logger(logging.INFO)
 #logger.info("Let's Begin")
 
@@ -48,65 +103,21 @@ sys.stdout.flush()
 
 #Just grab some Splunk Environment Info and log it
 splunk_home = os.path.expandvars("$SPLUNK_HOME")
-#logger.info("Splunk Home is:" + splunk_home)
 splunk_pid = open(os.path.join(splunk_home, "var", "run", "splunk", "conf-mutator.pid"), 'rb').read()
-#logger.info("Splunk PID is:" + splunk_pid)
 sessionKey = sys.stdin.readline().strip()
-#logger.info("variables initialized: splunk_home=" + splunk_home + " splunk_pid=" + splunk_pid)
 
-# start the real work
-# Read in all Access Tokens from nope.conf
+#Get the Splunk Tokens
+tokens = GetTokens()
+for key,value in tokens.iteritems():
+    value = json.loads(value)
+    logger.info(key+"'s Access Token is: "+value['APIKey'])
+    logger.info(key+"'s Refresh Token is: "+value['RefreshToken'])
+    #Refresh Token
+    #refreshed = RefreshToken(value['RefreshToken'])
+    
+    #Delete Token from REST
+    #DeleteToken(key):
 
-proc = []
-keys_dict = {}
-get_path = '/servicesNS/nobody/FitbitAddonforSplunk/storage/passwords?output_mode=json'
-try:
-    serverResponse = splunk.rest.simpleRequest(get_path, sessionKey=sessionKey, method='GET', raiseAllErrors=True)
-except Exception as e:
-    logger.info(str(e))
-jsonObj = json.loads(serverResponse[1])
-my_app = "FitbitAddonforSplunk"
-if len(jsonObj['entry']) == 0:
-    logger.warn("No credentials found.")
-    sleep(60)
-    sys.exit(0)
-else:
-    for entry in jsonObj['entry']:
-        if entry['acl']['app'] != my_app:
-            continue
-        #logger.info('Found a stanza for the Fitbit Add-on')
-        if 'clear_password' in entry['content'] and 'username' in entry['content']:
-            keys_dict[entry['content']['username']] = entry['content']['clear_password']
-            tokens = entry['content']['clear_password']
-            token_id = entry['content']['username']
-            
-tokens_json = jsonObj = json.loads(tokens)
-#Print Current Keys
-logger.info(tokens_json['APIKey'])
-logger.info(tokens_json['RefreshToken'])
+    #Update REST with new Token
+    #UpdateToken(refreshed, key)
 
-logger.info("Going to Bed")
-sleep(10)
-logger.info("waking up")
-
-try:
-    #Refresh Token 
-    req = urllib2.Request('https://5876gyevwj.execute-api.us-west-2.amazonaws.com/prod/refreshFitbitToken?code='+tokens_json['RefreshToken'])
-    response = urllib2.urlopen(req)
-    codes = json.loads(response.read())
-    logger.info(str(codes))
-except Exception as e:
-    logger.info(str(e))
-
-
-#Print for Diagnosis
-refreshed_tokens_json = codes
-logger.info(refreshed_tokens_json['APIKey'])
-logger.info(refreshed_tokens_json['RefreshToken'])
-
-json_built='{\"APIKey\":\"'+refreshed_tokens_json['APIKey']+'\",\"RefreshToken\":\"'+refreshed_tokens_json['RefreshToken']+'\"}'
-logger.info(json_built)
-
-
-
-#Post New Token To Splunk
